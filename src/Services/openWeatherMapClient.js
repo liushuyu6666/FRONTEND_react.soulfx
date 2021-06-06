e8458d96621313d8001b4ca710aac75d68352dcd
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-06-05 21:03:34 -0400

    change appid

--- src/Services/openWeatherMapClient.js
+++ src/Services/openWeatherMapClient.js
@@ -2,7 +2,7 @@ export const getWeather = () => {
     const data = {
         method: "GET"
     }
-    const url = "http://api.openweathermap.org/data/2.5/forecast?q=toronto&appid=083160e8fe8a9deda114381c598392d0"
+    const url = "http://api.openweathermap.org/data/2.5/forecast?q=toronto&appid=ef789d33883afc2c00eb596fd81b40cd"
     return fetch(url, data).then(response => {
         if(response.ok){
             return response.json();
