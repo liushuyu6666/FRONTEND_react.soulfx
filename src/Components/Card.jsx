791869f64a1f6bd17c62e32d0b1c0df23549ccfd
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-06-05 21:01:43 -0400

    use openweathermap itself's icon to associate the name of weather

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,18 +6,18 @@ import snow from '../svg/wi-day-snow.svg';
 import sunny from '../svg/wi-day-sunny.svg';
 
 export const Card = (props) => {
-    const map = {"cloudy": cloudy, "fog": fog, "rain": rain, "snow": snow, "sunny": sunny};
-    var weather = props.weather;
+    // const map = {"Clouds": cloudy, "fog": fog, "Rain": rain, "snow": snow, "sunny": sunny};
+    // var weather = props.weather;
     
     return (
         <div className={"weather-card"}>
-            <div className={"weather-card-first"}>
+            <div className={"weather-card-first"} key="1">
                 <label>{props.dt}</label>
             </div>
-            <div className={"weather-card-second"}>
-                <img src={map[weather]}></img>
+            <div className={"weather-card-second"} key="2">
+                <img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`}></img>
             </div>
-            <div className={"weather-card-third"}>
+            <div className={"weather-card-third"} key="3">
                 <label>{props.temp}</label>
             </div>
             
